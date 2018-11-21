aadb2c88448f9a9986ae6eb481cd3b8c78f43c0f
Leonardo Braz <lhleonardo@hotmail.com>
2018-11-21 11:33:16 -0200

    Criando centralização de operações do BD

---
+++ bd/operacoes-bd.php
@@ -0,0 +1,21 @@
+<?php
+    $acao = $_GET['action'];
+
+    switch ($acao) {
+        case 'INSERT':
+            # code...
+            break;
+        
+        case 'UPDATE':
+            break;
+        
+        case 'DELETE':
+            break;
+            
+        default:
+            # code...
+            break;
+    }
+
+
+?>
